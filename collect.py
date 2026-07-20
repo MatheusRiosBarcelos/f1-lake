a79086f33cb9258de36dbe9d24f46a21d60a72b1
MatheusRiosBarcelos <matheusriosbarcelos@gmail.com>
2026-07-20 19:39:27 -0300

    Estruturação da coleta dos dados

--- collect.py
+++ collect.py
@@ -2,24 +2,57 @@
 
 import fastf1
 import pandas as pd
+import time
+
 pd.set_option('display.max_columns', None)
 
 # %%
 
-for i in range(1, 50):
+class CollectResults:
+    
+    def __init__(self, years=[2021,2022,2023], modes=['R','S']):
+        self.years = years
+        self.modes = modes
+    
+    def get_data(self,year,gp,mode)->pd.DataFrame:
+        try:            
+            session = fastf1.get_session(year, gp, mode)
+        
+        except ValueError as err:
+            return pd.DataFrame()
+        
+        session._load_drivers_results()
+        
+        df = session.results
+        df["Mode"] = mode
+        
+        return df
+    
+    def save_data(self,df,year,gp,mode):
+        df.to_parquet(f"data/{year}_{gp:02}_{mode}.parquet")
+    
+    def process(self, year, gp, mode):
+        df = self.get_data(year, gp, mode)
+        
+        if df.empty:
+            return False
+        
+        self.save_data(df, year, gp, mode)
+        return True
+    
+    def process_year_modes(self, year):
+        for i in range(1, 50):
+            for mode in self.modes:
+                if not self.process(year, i, mode) and mode == 'R':
+                    return
 
-    print(f"Coletando GP {i:02}...")
-    
-    # Define a sessão de busca e carrega os dados
-    try:
-        session = fastf1.get_session(2021, i, 'R')
-    
-    except ValueError as err:
-        print(err)
-        break
+    def process_years(self):
+        for year in self.years:
+            self.process_year_modes(year)
+            time.sleep(10)
+      
+#%%
 
-    session._load_drivers_results()
-    
-    # Exibe e salva os dados obtidos
-    session.results
-    session.results.to_parquet(f"data/2021_{i:02}_R.parquet")
+collect = CollectResults([2023,2024,2025],['R','S'])
+collect.process_years()
+        
