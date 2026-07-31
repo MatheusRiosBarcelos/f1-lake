bd78e1bc8c77d129030b200c742de48ae1f7390c
MatheusRiosBarcelos <matheusriosbarcelos@gmail.com>
2026-07-31 19:15:03 -0300

    finish the model and starting with MlFlow

--- ml_champion/train.py
+++ ml_champion/train.py
@@ -1,7 +1,22 @@
 # %%
 
+import mlflow
 import pandas as pd
+
 from sklearn import model_selection
+from sklearn import ensemble
+from sklearn import metrics
+from sklearn import pipeline
+
+from feature_engine import imputation
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+#%%
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+mlflow.set_experiment(experiment_id=1)
 
 # %%
 
@@ -10,9 +25,29 @@ df.head()
 
 # %%
 
-df['year'] = df['dt_ref_life'].apply(lambda x: x.split('-')[0])
+### SEMMA
 
-df_driver_year = df[['driverid_life', 'year', 'flChampion']].drop_duplicates()
+#### SAMPLING
+
+df['year'] = df['dt_ref_life'].apply(lambda x: x.split('-')[0]).astype(int)
+df_oot = df[df['year'] == 2025].copy()
+df_oot
+
+df_analytics = df[df['year'] < 2025].copy()
+
+#%%
+
+df_year_round = df_analytics[['year', 'dt_ref_life']].drop_duplicates()
+df_year_round['row_number'] = (df_year_round.sort_values('dt_ref_life', ascending=False)
+                                            .groupby('year')
+                                            .cumcount())
+df_year_round[['dt_ref_life', 'year','row_number']]
+
+df_year_round = df_year_round[df_year_round['row_number'] > 4]
+df_year_round = df_year_round.drop('row_number', axis=1)
+df_year_round
+# %%
+df_driver_year = df_analytics[['driverid_life', 'year', 'flChampion']].drop_duplicates()
 df_driver_year.sort_values(['driverid_life', 'year'], ascending=[True,False])
 
 train, test = model_selection.train_test_split(
@@ -25,15 +60,11 @@ train, test = model_selection.train_test_split(
 print("Taxa de campeões treino", train['flChampion'].mean())
 print("Taxa de campeões teste", test['flChampion'].mean())
 
-# %%
-
-df_train = train.merge(df)
-df_test = test.merge(df)
+df_train = train.merge(df_analytics).merge(df_year_round, how = 'inner')
+df_test = test.merge(df_analytics).merge(df_year_round, how = 'inner')
 
 print("Quantidade de linhas train:", df_train.shape)
 print("Quantidade de linhas test:", df_test.shape)
-
-# %%
 
 features = df_train.columns[4:]
 features
@@ -41,6 +72,86 @@ features
 X_train, y_train  = df_train[features], df_train['flChampion']
 X_test, y_test  = df_test[features], df_test['flChampion']
 
+X_oot, y_oot  = df_oot[features], df_oot['flChampion']
+
 # %%
 
+#### EXPLORE
 
+isna = X_train.isna().sum()
+isna[isna > 0]
+
+# %%
+
+missing = imputation.ArbitraryNumberImputer(
+    arbitrary_number= -10000, 
+    variables= X_train.columns.tolist()
+    )
+
+X_train_transform = missing.fit_transform(X_train)
+
+# %%
+
+clf = ensemble.RandomForestClassifier(
+    min_samples_leaf=50, 
+    n_estimators=500,                                                                     
+    random_state=42,
+    n_jobs=4
+    )
+
+clf.fit(X_train_transform, y_train)
+
+# %%
+
+y_train_pred = clf.predict(X_train_transform)
+y_train_prob = clf.predict_proba(X_train_transform)[:,1]
+
+auc_train = metrics.roc_auc_score(y_train, y_train_prob)
+roc_train = metrics.roc_curve(y_train, y_train_prob)
+print("AUC Train:", auc_train)
+
+# %%
+X_test_transform = missing.fit_transform(X_test)
+
+y_test_pred = clf.predict(X_test_transform)
+y_test_prob = clf.predict_proba(X_test_transform)[:,1]
+
+auc_test = metrics.roc_auc_score(y_test, y_test_prob)
+roc_test = metrics.roc_curve(y_test, y_test_prob)
+print("AUC Test:", auc_test)
+
+# %%
+
+X_oot_transform = missing.fit_transform(X_oot)
+
+y_oot_pred = clf.predict(X_oot_transform)
+y_oot_prob = clf.predict_proba(X_oot_transform)[:,1]
+
+auc_oot = metrics.roc_auc_score(y_oot, y_oot_prob)
+roc_oot = metrics.roc_curve(y_oot, y_oot_prob)
+print("AUC OOT:", auc_oot)
+
+# %%
+
+import matplotlib.pyplot as plt
+
+plt.plot(roc_train[0], roc_train[1])
+plt.plot(roc_test[0], roc_test[1])
+plt.plot(roc_oot[0], roc_oot[1])
+plt.legend([f"Treino: {auc_train:.4f}", f"Teste: {auc_test:.4f}", f"OOT: {auc_oot:.4f}"])
+plt.grid(True)
+plt.title("Curva ROC")
+
+# %%
+
+feature_importance = pd.Series(clf.feature_importances_, index=X_train_transform.columns)
+feature_importance = feature_importance.sort_values(ascending=False)
+feature_importance.head(20)
+
+# %%
+
+df_oot['pred'] = y_oot_prob
+
+df_oot[['driverid_life', 'dt_ref_life', 'flChampion', 'pred']].sort_values(['dt_ref_life','pred'], ascending=False)
+
+# %%
